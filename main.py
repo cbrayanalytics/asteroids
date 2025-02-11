b4733423f16301b0d2db8b4ae23d39558839889b
Casey Bray <cbrayanalytics@gmail.com>
2025-02-11 00:02:55 -0700

    Add game loop

--- main.py
+++ main.py
@@ -2,10 +2,25 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 
 def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+
     print("Starting asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":

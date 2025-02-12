9b6b66cafbd8d381fb54d8b97290395a81e7f4bf
Casey Bray <cbrayanalytics@gmail.com>
2025-02-11 20:44:39 -0700

    Add player and group mechanics

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -13,12 +14,23 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
         screen.fill("black")
+        for d in drawable:
+            d.draw(screen)
+
         pygame.display.flip()
 
         clock.tick(60)
